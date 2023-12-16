811590dbd188835de11671a9652a12be78d6ab7f
Yuanchao Sun <yuanchao.sun@gmail.com>
2023-12-16 22:33:14 +0800

    Update init script

--- scripts/init_and_start.sh
+++ scripts/init_and_start.sh
@@ -19,6 +19,12 @@ if [ ! -f "$DATA_DIR/config/config.toml" ]; then
 	# Download genesis file
 	curl -L -o $DATA_DIR/config/genesis.json $GENESIS_FILE
 
+	# Get the IBC_TOKEN_DENOM from genesis.json
+	IBC_TOKEN_DENOM=`jq -r '.app_state.crisis.constant_fee.denom' $DATA_DIR/config/genesis.json`
+
+	# Modify the minimum-gas-price field of app.toml
+	sed -i.bak "s#minimum-gas-prices = \"0aotto\"#minimum-gas-prices = \"20000000000${IBC_TOKEN_DENOM}\"#" $DATA_DIR/config/app.toml
+
 	# Copy cosmovisor folder to data directory
 	cp -R /root/cosmovisor $DATA_DIR/
 
